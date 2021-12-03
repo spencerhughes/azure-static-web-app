529583a35207a6440b96eba5f1b6468d3e3509be
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2021-12-03 15:23:50 -0500

    Adds a button

---
+++ button.js
@@ -0,0 +1,10 @@
+function mouseDown(){
+  var paragraph = document.getElementById("p")
+
+paragraph.innerText = "Button!"
+}
+
+function mouseUp(){
+  var paragraph = document.getElementById("p")
+  paragraph.innerText = ""
+}
